926e6ff0a0f5c5aded45b9b9af9201145d82145a
Alexander Meléndez <xandermelendez.2001@gmail.com>
2021-11-22 20:16:41 -0500

    Ultimos cambios

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -121,8 +121,14 @@ public class PlayerController : MonoBehaviour
             TimeController.timeController.disminucion(5);
         }
         else if (collision.CompareTag("Correct"))
-        {
-            SceneManager.LoadScene("Level_2", LoadSceneMode.Single);
+        {   
+            if (Application.loadedLevelName == "Level_1")  SceneManager.LoadScene("Level_2", LoadSceneMode.Single);
+            else if (Application.loadedLevelName == "Level_2") SceneManager.LoadScene("Level_3", LoadSceneMode.Single);
+            else if (Application.loadedLevelName == "Level_3") SceneManager.LoadScene("Level_4", LoadSceneMode.Single);
+            else if (Application.loadedLevelName == "Level_4") SceneManager.LoadScene("Level_5", LoadSceneMode.Single);
+            else if (Application.loadedLevelName == "Level_5") SceneManager.LoadScene("Level_6", LoadSceneMode.Single);
+            else if (Application.loadedLevelName == "Level_6") SceneManager.LoadScene("Level_7", LoadSceneMode.Single);
+            else if (Application.loadedLevelName == "Level_7") SceneManager.LoadScene("Level_8", LoadSceneMode.Single);
             print("Colision - Gano el juego");
         }
         else if (collision.CompareTag("Coin"))
